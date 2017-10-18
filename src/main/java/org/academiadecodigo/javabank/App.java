192aee7923204ab62bac784fb0ee14befd1ce554
Francisca Quadros <fqfrancisca@gmail.com>
2017-10-18 15:00:36 +0100

    Working version with spring

--- src/main/java/org/academiadecodigo/javabank/App.java
+++ src/main/java/org/academiadecodigo/javabank/App.java
@@ -1,16 +1,11 @@
 package org.academiadecodigo.javabank;
 
 import org.academiadecodigo.javabank.controller.Controller;
+import org.academiadecodigo.javabank.controller.LoginController;
 import org.academiadecodigo.javabank.persistence.H2WebServer;
-import org.academiadecodigo.javabank.persistence.SessionManager;
 import org.academiadecodigo.javabank.persistence.TransactionManager;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaAccountDao;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaCustomerDao;
-import org.academiadecodigo.javabank.persistence.jpa.JpaSessionManager;
-import org.academiadecodigo.javabank.persistence.jpa.JpaTransactionManager;
-import org.academiadecodigo.javabank.services.AccountServiceImpl;
-import org.academiadecodigo.javabank.services.AuthServiceImpl;
-import org.academiadecodigo.javabank.services.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -25,14 +20,13 @@ public class App {
             H2WebServer h2WebServer = new H2WebServer();
             h2WebServer.start();
 
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory(Config.PERSISTENCE_UNIT);
-            JpaSessionManager sm = new JpaSessionManager(emf);
-            TransactionManager tx = new JpaTransactionManager(sm);
+            ApplicationContext context = new ClassPathXmlApplicationContext(
+                    new String[] {"META-INF/persistence/persistencecs.xml"}
+            );
 
-            App app = new App();
-            app.bootStrap(tx, sm);
+            LoginController h = context.getBean("loginController", LoginController.class);
 
-            emf.close();
+            h.init();
             h2WebServer.stop();
 
         } catch (SQLException ex) {
@@ -40,25 +34,4 @@ public class App {
         }
     }
 
-    private void bootStrap(TransactionManager tx, JpaSessionManager sm) {
-
-        AccountServiceImpl accountService = new AccountServiceImpl();
-        accountService.setAccountDao(new JpaAccountDao(sm));
-        accountService.setTransactionManager(tx);
-
-        CustomerServiceImpl customerService = new CustomerServiceImpl();
-        customerService.setCustomerDao(new JpaCustomerDao(sm));
-        customerService.setTransactionManager(tx);
-
-        Bootstrap bootstrap = new Bootstrap();
-
-        bootstrap.setAuthService(new AuthServiceImpl());
-        bootstrap.setAccountService(accountService);
-        bootstrap.setCustomerService(customerService);
-
-        Controller controller = bootstrap.wireObjects();
-
-        // start application
-        controller.init();
-    }
 }
