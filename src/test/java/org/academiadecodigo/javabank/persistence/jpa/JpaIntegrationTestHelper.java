2c31481bfaff635bc0673c6494e62462d863012a
Francisca Quadros <fqfrancisca@gmail.com>
2017-10-20 11:06:42 +0100

    first commit

--- src/test/java/org/academiadecodigo/javabank/persistence/jpa/JpaIntegrationTestHelper.java
+++ src/test/java/org/academiadecodigo/javabank/persistence/jpa/JpaIntegrationTestHelper.java
@@ -1,44 +1,36 @@
 package org.academiadecodigo.javabank.persistence.jpa;
 
-import org.academiadecodigo.javabank.Config;
 import org.junit.After;
 import org.junit.Before;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class JpaIntegrationTestHelper {
 
     protected EntityManagerFactory emf;
-    protected JpaSessionManager sm;
-    protected JpaTransactionManager tx;
+    protected EntityManager em;
 
     @Before
     public void init() {
 
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
         ctx.getEnvironment().setActiveProfiles("test");
-        ctx.load(Config.SPRING_CONFIG);
+        ctx.load("file:src/main/webapp/WEB-INF/spring/spring-config.xml");
         ctx.refresh();
 
         emf = ctx.getBean(EntityManagerFactory.class);
+        em = emf.createEntityManager();
 
-        sm = new JpaSessionManager();
-        tx = new JpaTransactionManager();
-
-        sm.setEmf(emf);
-        tx.setSm(sm);
-
-        tx.beginRead();
     }
 
     @After
     public void tearDown() {
 
-        if (sm.getCurrentSession().getTransaction().getRollbackOnly()) {
-            tx.rollback();
-        } else {
-            tx.commit();
+        if (em != null) {
+            em.clear();
+            em.close();
         }
 
         if (emf != null) {
